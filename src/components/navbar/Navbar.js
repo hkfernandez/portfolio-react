b4f8dd6cfb53c2256f5e773bd1fc218b875f5d1c
Hector Fernandez <hkfernandezdev@gmail.com>
2021-03-07 17:58:49 -0600

    Update navbar style and links
    changed paths to exact paths to make the links work - updated basic styling and flexbox functionality

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,40 +1,46 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import About from '../about/About'
-import Portfolio from '../portfolio/Portfolio'
-import Contact from '../contact/Contact'
+import About from '../about/About';
+import Portfolio from '../portfolio/Portfolio';
+import Contact from '../contact/Contact';
+import './navbar.css';
 
-function Navbar () {
-  return (
+function clickTest() {
+	console.log('click');
+}
+
+function Navbar() {
+	return (
 		<Router>
-		<div>
-			<nav>
-			<ul>
-				<li>
-				<Link to="/">About</Link>
-				</li>
-				<li>
-				<Link to="/portfolio">Portfolio</Link>
-				</li>
-				<li>
-				<Link to="/contact">Contact</Link>
-				</li>
-			</ul>
-			</nav>
+			<div>
+				<nav className='navbar'>
+					<h3>Hector Fernandez</h3>
+					<ul className='list'>
+						<li>
+							<Link to="/" onClick={clickTest}>About</Link>
+						</li>
+						<li>
+							<Link to="/portfolio" onClick={clickTest}>Portfolio</Link>
+						</li>
+						<li>
+							<Link to="/contact">Contact</Link>
+						</li>
+					</ul>
+				</nav>
 
-			<Switch>
-			<Route path="/">
-				<About />
-			</Route>
-			<Route path="/portfolio">
-				<Portfolio />
-			</Route>
-			<Route path="/contact">
-				<Contact />
-			</Route>
-			</Switch>
-		</div>
+				<Switch>
+					<Route exact path="/">
+						<About />
+					</Route>
+					<Route exact path="/portfolio">
+						<Portfolio />
+					</Route>
+					<Route exact path="/contact">
+						<Contact />
+					</Route>
+				</Switch>
+			</div>
 		</Router>
-  );
+	);
 }
 export default Navbar;
