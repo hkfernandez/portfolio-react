17842ecf45cf2b2a9e18ba1c8b298a49396567c3
Hector Fernandez <hkfernandezdev@gmail.com>
2021-03-09 22:59:24 -0600

    Add Footer Component
    added footer and corresponding CSS

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './footer.css';
+
+function Footer() {
+	return (
+		<footer>
+			<p>React - Javascript - HTML - CSS - Handlebars - jQuery - Node.js - Express - mySQL - Sequelize - Mongodb - Moongoose</p>
+		</footer>
+	);
+}
+export default Footer;
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -6,6 +6,7 @@ import {
 import About from '../about/About';
 import Portfolio from '../portfolio/Portfolio';
 import Contact from '../contact/Contact';
+import Footer from '../footer/Footer';
 import './navbar.css';
 
 
@@ -14,40 +15,43 @@ function Navbar() {
 	// const location = useLocation();
 
 	return (
-		<Router>
-			<div>
-				<nav className='navbar'>
-					<h3 className='hector'>Hector Fernandez</h3>
-					<ul className='list'>
-						<li>
-							<Link to="/"
-							// className={location.pathname === "/" ? "nav-link active" : "nav-link"}
-							>
-								{`[ About ]`}
-							</Link>
-						</li>
-						<li>
-							<Link to="/portfolio">{`[ Portfolio ]`}	</Link>
-						</li>
-						<li>
-							<Link to="/contact">{`[ Contact ]`}</Link>
-						</li>
-					</ul>
-				</nav>
+		<>
+			<Router>
+				<div>
+					<nav className='navbar'>
+						<h3 className='hector'>Hector Fernandez</h3>
+						<ul className='list'>
+							<li>
+								<Link to="/"
+								// className={location.pathname === "/" ? "nav-link active" : "nav-link"}
+								>
+									{`[ About ]`}
+								</Link>
+							</li>
+							<li>
+								<Link to="/portfolio">{`[ Portfolio ]`}	</Link>
+							</li>
+							<li>
+								<Link to="/contact">{`[ Contact ]`}</Link>
+							</li>
+						</ul>
+					</nav>
 
-				<Switch>
-					<Route exact path="/">
-						<About />
-					</Route>
-					<Route exact path="/portfolio">
-						<Portfolio />
-					</Route>
-					<Route exact path="/contact">
-						<Contact />
-					</Route>
-				</Switch>
-			</div>
-		</Router>
+					<Switch>
+						<Route exact path="/">
+							<About />
+						</Route>
+						<Route exact path="/portfolio">
+							<Portfolio />
+						</Route>
+						<Route exact path="/contact">
+							<Contact />
+						</Route>
+					</Switch>
+				</div>
+			</Router>
+			<Footer />
+		</>
 	);
 }
 export default Navbar;
