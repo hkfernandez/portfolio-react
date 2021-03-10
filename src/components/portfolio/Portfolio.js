9466f855c196c9fa830398f816756c8989e5c6e2
Hector Fernandez <hkfernandezdev@gmail.com>
2021-03-09 20:13:26 -0600

    Add readme and Update CSS
    updated CSS on portfolio conponent

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,23 +1,25 @@
 import React from 'react';
-import projectsArr from './projectsList.json'
-
+import projectsArr from './projectsList'
+import './portfolio.css'
 
 
 function Portfolio() {
 
 	const projects = projectsArr
+	console.log(projects);
 
 	return (
-		<main>
+		<main className='portfolioWrapper'>
 			{
 				projects.map(
-					(project) => {
+					(project, index) => {
 						return (
-							<section>
-								<img src={project.image} alt={project.atlText} />
+							<section key={index} className='project'>
+								<img src={project.imageSource} alt={project.atlText} />
+								<p>{project.text}</p>
 								<a href={project.depolyedLink}>{`[ Website ]`}</a>
 								<a href={project.githubLink}>{`[ GitHub Repo ]`}</a>
-								<p>{project.text}</p>
+								<p>Key Tech: {project.tech}</p>
 							</section>
 						)
 					}
---
+++ src/components/portfolio/projectsList.js
@@ -0,0 +1,83 @@
+import stockAppImg from './images/stocks.png'
+import weatherAppImg from './images/weatherDashboardImg.png'
+import recipeAppImg from './images/recipeAppImg.png'
+import scheduleAppImg from './images/appImage.png'
+import burgerAppImg from './images/burger.png'
+import teamAppImg from './images/team.png'
+import portfolioAppImg from './images/portfolio.png'
+
+
+const projectsArr = [
+	{
+		imageSource: stockAppImg,
+		atlText: "Investing Web Application",
+		depolyedLink: "https://secret-cliffs-66187.herokuapp.com/",
+		githubLink: "https://github.com/bardeeens/stock-simulator",
+		text: "Learn about the stock market by trading risk free and following real time share prices.",
+		tech: [
+			"Javascript",
+			"jQuery",
+			"Node.js",
+			"mySQL & Sequelize",
+			"3rd Party API - FMP"
+		]
+	},
+	{
+		imageSource: weatherAppImg,
+		atlText: "Weather Web Application",
+		depolyedLink: "https://hkfernandez.github.io/weather_dashboard/class=",
+		githubLink: "https://github.com/hkfernandez/weather_dashboard",
+		text: "A simple way to stay up to date on the current weather in different cities.",
+		tech: [
+			"Javascript",
+			"jQuery",
+			"Node.js",
+			"LocalStorage"
+		]
+	},
+	{
+		imageSource: recipeAppImg,
+		atlText: "Recipe Finder Web Application",
+		depolyedLink: "https://dorykahale.github.io/Recipe-Saver/",
+		githubLink: "https://github.com/hkfernandez/Recepi",
+		text: "Find new recipe ideas in both English and Spanish and keep your favorites in one clean interface.",
+		tech: [
+			"Javascript",
+			"jQuery",
+			"Node.js",
+			"UI-kit CSS Library",
+			"Express Server",
+			"mySQL & Sequelize",
+			"Third Party API's Edamam & Spoonacular"
+		]
+	},
+	{
+		imageSource: burgerAppImg,
+		atlText: "Hamburger Game Web Application",
+		depolyedLink: "https://quiet-bayou-27411.herokuapp.com/",
+		githubLink: "https://github.com/hkfernandez/Burger",
+		text: "A fun, nonsensical game where the player can eat as many delicious burgers as they want without feeling bad.",
+		tech: [
+			"Javascript",
+			"jQuery",
+			"Node.js",
+			"Vanilla CSS",
+			"Handlebars",
+			"Express Server",
+			"mySQL"
+		]
+	},
+	{
+		imageSource: teamAppImg,
+		atlText: "Team Directory Web Application",
+		depolyedLink: "https://github.com/hkfernandez/employee-directory",
+		githubLink: "https://hkfernandez.github.io/employee-directory/",
+		text: "This app allows access to a list of team members and their contact information. The list can be dynamically filtered when searching and sorted by team member name.",
+		tech: [
+			"React",
+			"Express Server",
+		]
+	},
+]
+
+export default projectsArr;
