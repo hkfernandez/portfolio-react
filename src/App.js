e9164f70d4d37575e606fffa5a0ab55496838db6
Hector Fernandez <hkfernandezdev@gmail.com>
2021-03-24 17:32:53 -0500

    Update to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './text.css'
 import './App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import Navbar from './components/navbar/Navbar'
 import Footer from './components/footer/Footer'
 import About from './components/about/About'
@@ -12,7 +12,6 @@ function App() {
 	return (
 		<Router>
 			<Navbar />
-			<Route exact path="" component={About} />
 			<Route exact path="/" component={About} />
 			<Route exact path="/contact" component={Contact} />
 			<Route exact path="/portfolio" component={Portfolio} />
